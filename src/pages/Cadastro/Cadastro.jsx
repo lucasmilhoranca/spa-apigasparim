309f430832c46cac86a97951e7af40fb9623aa0a
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-01-23 09:27:22 -0300

    feat: criando pagina de cadastro e componente de formulário

---
+++ src/pages/Cadastro/Cadastro.jsx
@@ -0,0 +1,15 @@
+import { Navbar } from "../../components/Navbar/Navbar";
+import { CadBody, CadContainer } from "./CadastroStyled";
+
+export default function Cadastro() {
+    return (
+        <>
+            <Navbar />
+            <CadBody>
+                <CadContainer>
+                    
+                </CadContainer>
+            </CadBody>
+        </>
+    )
+}
---
+++ src/pages/Cadastro/CadastroStyled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const CadBody = styled.section`
+    display: flex;
+    `;
+
+export const CadContainer = styled.section`
+    display: flex;
+    `;
