7777e9391c84e8c10e736113afeb5bd0b29428a2
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-01-25 15:35:10 -0300

    refact: colocando h2 em cor verde

--- src/pages/Cadastro/CadastroStyled.jsx
+++ src/pages/Cadastro/CadastroStyled.jsx
@@ -12,6 +12,9 @@ export const CadBody = styled.section`
     border-radius: 1rem;
     box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
 
+    h2 {
+        color: #2eb451;
+    }
 `;
 
 export const CadContainer = styled.section`
