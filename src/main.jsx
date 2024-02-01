5c18bd80bc3205c75e2109853d2c8291be93a57c
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-02-01 14:11:01 -0300

    chore: retirando react helmet e colocando importação de fonte direto no index.html

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,6 @@ import { Auth } from './pages/Authentication/Auth.jsx'
 import Cadastro from './pages/Cadastro/Cadastro.jsx'
 import Check from './pages/Check/Check.jsx'
 import UserProvider from './Context/userContext.jsx'
-import { Helmet } from 'react-helmet'
 
 const router = createBrowserRouter([
 	{
@@ -44,9 +43,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Helmet>
-			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&family=Sora:wght@100..800&display=swap" />
-		</Helmet>
 		<GlobalStyle />
 		<UserProvider>
 			<RouterProvider router={router} />
