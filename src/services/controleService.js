c1ce98688b0eab060bc4ba843c56df765eeb3bb7
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-02-19 09:08:50 -0300

    feat: criando searchStatus para o axios

--- src/services/controleService.js
+++ src/services/controleService.js
@@ -3,11 +3,19 @@ import axios from "axios";
 const baseURL = "http://localhost:3000"
 
 export function getAllChecks() {
-    const response = axios.get(`${baseURL}/control`); //async
+    const response = axios.get(`${baseURL}/control`);
+
     return response;
 }
 
 export function searchChecks(cpf) {
-    const response = axios.get(`${baseURL}/control/${cpf}`); //async
+    const response = axios.get(`${baseURL}/control/${cpf}`);
+
+    return response;
+}
+
+export function searchStatus(cpf) {
+    const response = axios.get(`${baseURL}/control/${cpf}/status`);
+    
     return response;
 }
